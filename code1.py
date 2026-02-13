ed4351831205cab57d9129d16e00763b8ee1deaa
Clint John D. Pepito <Pepito29.clintjohn@gmail.com>
2026-02-13 10:02:51 +0800

    first push

---
+++ code1.py
@@ -0,0 +1,9 @@
+Username = "Clint John Pepito"
+edad = "20"
+height = "165cm"
+v2 = False
+
+print("Pangalan sa studyante: ",Username, "->", type(Username))
+print("edad: ",edad, "->", type(edad))
+print("height: ",height, "->", type(height))
+print("Nag trabaho karun: ",v2, "->", type(v2))
